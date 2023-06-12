d20f3e6864d82a3ea8ff097c30fb3a708263b8df
a60156077 <dwrhksfl77@naver.com>
2023-06-12 17:50:44 +0900

    캐스팅, 제목, 기타 정보 가져오는지 테스트 필요..

--- crawler/src/test/kotlin/com/arton/crawler/CrawlerApplicationTests.kt
+++ crawler/src/test/kotlin/com/arton/crawler/CrawlerApplicationTests.kt
@@ -37,13 +37,40 @@ class CrawlerApplicationTests {
 
 		val baseUrl: String = "https://tickets.interpark.com/goods/23004388"
 
-		val contents = mutableListOf<String>()
 		try{
 			driver.get(baseUrl)
+
+			// find title
+			val titleElement = driver.findElement(By.xpath("//h2[contains(text(), 'prdTitle')]"))
+			if (titleElement != null) {
+				println("titleElement = ${titleElement.text}")
+			} else {
+				println("title not found")
+			}
+			// find summary info
+			val summaryElement = driver.findElement(By.xpath("//div[contains(text(), 'summaryBody')]"))
+			if (summaryElement != null) {
+				val imgElement =
+					summaryElement.findElement(By.xpath("//div[@class='posterBox']/div[@class='posterBoxTop']/img"))
+				if (imgElement != null) {
+					val src = imgElement.getAttribute("src").toString()
+					val imageUrl = "https:" + src
+					println("imageUrl = ${imageUrl}")
+				}
+				val summaryList = summaryElement.findElement(By.xpath("//ul[contains(text(), 'info']"))
+				if (summaryList != null) {
+					val summaryLis = summaryList.findElements(By.xpath("li"))
+					for (summaryLi in summaryLis) {
+						println("summaryLi = ${summaryLi.text}")
+					}
+				}
+			}
+
+
 			// find casting content
 			val castingElement: WebElement? = driver.findElement(By.xpath("//div[contains(text(), 'content casting')]"))
 			if (castingElement != null) {
-				val castingList = castingElement.findElement(By.xpath("//div/ul[contains(text(), castingList]"))
+				val castingList = castingElement.findElement(By.xpath("//div/ul[contains(text(), 'castingList')]"))
 				if (castingList != null) {
 					val listItems: List<WebElement> = castingList.findElements(By.tagName("li"))
 					for (listItem in listItems) {
@@ -51,6 +78,7 @@ class CrawlerApplicationTests {
 					}
 				}
 			}
+
 		}catch (e: Exception){
 			e.printStackTrace()
 		}finally {
